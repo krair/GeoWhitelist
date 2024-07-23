cd20b3ebbc2894a08a9d98a816a932b5629a3e6b
Kit <kit@krairigh.com>
2024-07-23 15:20:35 +0200

    tests for the CheckIP function

--- tests/unit/test_check_ip.py
+++ tests/unit/test_check_ip.py
@@ -3,11 +3,24 @@ Unit tests for checkIP function of GeoWhitelist
 """
 import pytest
 from geowhitelist import checkIP
-#target = __import__("geowhitelist.py")
-#checkIP = target.checkIP
 
 
-def test_bad_ip():
-    data = "123.456.789.012"
-    with pytest.raises(ValueError):
-        checkIP(data)
+@pytest.mark.parametrize("maybe_ip, expected_result", [
+    ("", False),
+    ("not an IP", False),
+    ("10.0.0.1", True),
+    ("6.7.8.9", True),
+    (['1.2.3.4', '4.6.7.8'], False),
+    (None, False),
+])
+@pytest.mark.asyncio
+async def test_bad_ip(
+                      maybe_ip,
+                      expected_result,
+                      monkeypatch,
+                      mocker
+                      ):
+    mock_access_control = mocker.patch('geowhitelist.accessControl',
+                                       return_value=True)
+    res = await checkIP(maybe_ip)
+    assert res == expected_result
